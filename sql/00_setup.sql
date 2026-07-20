345c3cfcacd49adb377b11af44280cad742d07a6
Dawid Kostrzewa <dawid.kostrzewa00@icloud.com>
2026-07-20 12:38:49 +0200

    Add USE statement to setup script

--- sql/00_setup.sql
+++ sql/00_setup.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE olist_dwh;
 GO
+USE olist_dwh;
+GO
 CREATE SCHEMA raw;
 GO
 CREATE SCHEMA stg;
@@ -19,4 +21,47 @@ CREATE TABLE raw.orders (
     order_delivered_carrier_date NVARCHAR(MAX),
     order_delivered_customer_date NVARCHAR(MAX),
     order_estimated_delivery_date NVARCHAR(MAX)
+);
+GO
+
+DROP TABLE IF EXISTS raw.customers;
+CREATE TABLE raw.customers (
+    customer_id NVARCHAR(MAX),
+    customer_unique_id NVARCHAR(MAX),
+    customer_zip_code_prefix NVARCHAR(MAX),
+    customer_city NVARCHAR(MAX),
+    customer_state NVARCHAR(MAX)
+);
+GO
+
+DROP TABLE IF EXISTS raw.order_items;
+CREATE TABLE raw.order_items (
+    order_id NVARCHAR(MAX),
+    order_item_id NVARCHAR(MAX),
+    product_id NVARCHAR(MAX),
+    seller_id NVARCHAR(MAX),
+    shipping_limit_date NVARCHAR(MAX),
+    price NVARCHAR(MAX),
+    freight_value NVARCHAR(MAX)
+);
+GO
+
+DROP TABLE IF EXISTS raw.products;
+CREATE TABLE raw.products (
+    product_id NVARCHAR(MAX),
+    product_category_name NVARCHAR(MAX),
+    product_name_lenght NVARCHAR(MAX),
+    product_description_lenght NVARCHAR(MAX),
+    product_photos_qty NVARCHAR(MAX),
+    product_weight_g NVARCHAR(MAX),
+    product_length_cm NVARCHAR(MAX),
+    product_height_cm NVARCHAR(MAX),
+    product_width_cm NVARCHAR(MAX)
+);
+GO
+
+DROP TABLE IF EXISTS raw.product_category_name_translation;
+CREATE TABLE raw.product_category_name_translation (
+    product_category_name NVARCHAR(MAX),
+    product_category_name_english NVARCHAR(MAX)
 );
